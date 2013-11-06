988b54b205299ea26e056d21601c48c2c3df2682
dimawhisper <daeron.org@gmail.com>
2013-11-06 16:32:11 +0400

    2
    2

--- Menu.php
+++ Menu.php
@@ -1,9 +1,5 @@
 <?php
-
-/**
- * This is the model class for table "menu".
- *
- * The followings are the available columns in table 'menu':
+/*
  * @property integer $id
  * @property string $type
  * @property integer $id_item
@@ -12,55 +8,33 @@
  */
 class Menu extends CActiveRecord
 {
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return Menu the static model class
-	 */
+
 	public static function model($className=__CLASS__)
 	{
 		return parent::model($className);
 	}
 
-	/**
-	 * @return string the associated database table name
-	 */
 	public function tableName()
 	{
 		return 'menu';
 	}
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
 	public function rules()
 	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
 		return array(
 			array('type, id_item, weight, id_parent', 'required'),
 			array('id_item, weight, id_parent', 'numerical', 'integerOnly'=>true),
 			array('type', 'length', 'max'=>255),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
 			array('id, type, id_item, weight, id_parent', 'safe', 'on'=>'search'),
 		);
 	}
 
-	/**
-	 * @return array relational rules.
-	 */
 	public function relations()
 	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
 		return array(
 		);
 	}
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
 	public function attributeLabels()
 	{
 		return array(
@@ -122,25 +96,17 @@ class Menu extends CActiveRecord
         return $ret;
     }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    public function search(){
 
-		$criteria=new CDbCriteria;
+	$criteria=new CDbCriteria;
+	$criteria->compare('id',$this->id);
+	$criteria->compare('type',$this->type,true);
+	$criteria->compare('id_item',$this->id_item);
+	$criteria->compare('weight',$this->weight);
+	$criteria->compare('id_parent',$this->id_parent);
 
-		$criteria->compare('id',$this->id);
-		$criteria->compare('type',$this->type,true);
-		$criteria->compare('id_item',$this->id_item);
-		$criteria->compare('weight',$this->weight);
-		$criteria->compare('id_parent',$this->id_parent);
-
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+	return new CActiveDataProvider($this, array(
+		'criteria'=>$criteria,
+	));
+    }
 }
